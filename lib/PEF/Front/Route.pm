bb76b9e9dfce8c6759f58976c8f1a6116db86d15
Anton Petrusevich <anton.petrusevich@lycos.com>
2016-07-13 09:57:51 +0200

    simplified subrequest handling, rename subrequest -> process_request

--- lib/PEF/Front/Route.pm
+++ lib/PEF/Front/Route.pm
@@ -231,20 +231,6 @@ sub www_static_handler {
 	}
 }
 
-sub subrequest {
-	my $request       = $_[0];
-	my $http_response = rewrite_route($request);
-	cfg_log_level_info
-		&& $request->logger->({level => "info", message => "serving sub-request: " . $request->path});
-	return $http_response if $http_response;
-	my $context = prepare_context($request);
-	if (blessed($context) && $context->isa('PEF::Front::Response')) {
-		return $context;
-	}
-	$context->{is_subrequest} = 1;
-	PEF::Front::Ajax::handler($request, $context);
-}
-
 my %handlers = (
 	'/app'    => \&PEF::Front::RenderTT::handler,
 	'/ajax'   => \&PEF::Front::Ajax::handler,
@@ -252,44 +238,49 @@ my %handlers = (
 	'/submit' => \&PEF::Front::Ajax::handler,
 );
 
+sub process_request {
+	my ($request, $parent_context) = @_;
+	cfg_log_level_info
+		&& $request->logger->({level => "info", message => "serving request: " . $request->path});
+	my $http_response = rewrite_route($request);
+	return $http_response if $http_response;
+	if (cfg_url_contains_lang
+		&& (length($request->path) < 4 || substr($request->path, 3, 1) ne '/'))
+	{
+		my $lang = PEF::Front::NLS::guess_lang($request);
+		if ($request->method eq 'GET') {
+			$http_response = PEF::Front::Response->new(request => $request);
+			$http_response->redirect("/$lang" . $request->request_uri);
+			return $http_response->response();
+		} else {
+			$request->path("/$lang" . $request->path);
+		}
+	}
+	my $lang_offset = (cfg_url_contains_lang) ? 3 : 0;
+	my $handler;
+	for my $prefix (keys %handlers) {
+		if (substr($request->path, $lang_offset, length $prefix) eq $prefix) {
+			$handler = $handlers{$prefix};
+			last;
+		}
+	}
+	if ($handler) {
+		my $context = prepare_context($request);
+		if (blessed($context) && $context->isa('PEF::Front::Response')) {
+			return $context->response();
+		}
+		$context->{parent_context} = $parent_context;
+		$handler->($request, $context);
+	} else {
+		$http_response = PEF::Front::Response->new(request => $request, status => 404);
+		www_static_handler($request, $http_response) if cfg_handle_static;
+		$http_response->response();
+	}
+}
+
 sub to_app {
 	sub {
-		my $request       = PEF::Front::Request->new($_[0]);
-		my $http_response = rewrite_route($request);
-		cfg_log_level_info
-			&& $request->logger->({level => "info", message => "serving request: " . $request->path});
-		return $http_response->response() if $http_response;
-		if (cfg_url_contains_lang
-			&& (length($request->path) < 4 || substr($request->path, 3, 1) ne '/'))
-		{
-			my $lang = PEF::Front::NLS::guess_lang($request);
-			if ($request->method eq 'GET') {
-				$http_response = PEF::Front::Response->new(request => $request);
-				$http_response->redirect("/$lang" . $request->request_uri);
-				return $http_response->response();
-			} else {
-				$request->path("/$lang" . $request->path);
-			}
-		}
-		my $lang_offset = (cfg_url_contains_lang) ? 3 : 0;
-		my $handler;
-		for my $prefix (keys %handlers) {
-			if (substr($request->path, $lang_offset, length $prefix) eq $prefix) {
-				$handler = $handlers{$prefix};
-				last;
-			}
-		}
-		if ($handler) {
-			my $context = prepare_context($request);
-			if (blessed($context) && $context->isa('PEF::Front::Response')) {
-				return $context->response();
-			}
-			$handler->($request, $context);
-		} else {
-			$http_response = PEF::Front::Response->new(request => $request, status => 404);
-			www_static_handler($request, $http_response) if cfg_handle_static;
-			$http_response->response();
-		}
+		process_request(PEF::Front::Request->new($_[0]));
 	};
 }
 1;
